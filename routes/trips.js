a31ab4eff29e62603db570e93c3f9367182df678
Christian Larcher <cdrc.larcher@gmail.com>
2022-11-09 14:06:06 +0100

    route search ok

--- routes/trips.js
+++ routes/trips.js
@@ -2,17 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 const Trip = require('../models/trips')
+let moment = require('moment');
 
-// Rechercher ds la bdd un trajet
+
+// Rechercher ds la bdd un trajet via route /trips
 router.post('/', (req,res)=> {
-  Trip.find({departure:req.body.departure, arrival:req.body.arrival}/*, date:req.body.date}*/).then(dbData => {
-    if (dbData.length > 0) {
-      res.json({trips : dbData});
-      console.log(dbData);
-    } else {
-      res.json({result: false, error : 'This trip does\'nt exist'})
+  const {departure, arrival, date} = req.body;
+  //departure = departure[0].toUpperCase()+departure.slice(1,departure.length-1).toLowerCase;
+  //arrival=arrival[0].toUpperCase()+arrival.slice(1,arrival.length-1).toLowerCase;
+
+  // ------------ Principe : créer limites du jour date 
+  // date1 : début du jour date
+  console.log(date);
+  const date1 = new Date(date);
+  console.log(date1)
+  // date2 : fin du jour date
+  const date2Nb = date1.getTime() + 864e5;
+  const date2 = new Date(date2Nb);
+  console.log(date2)
+  
+  
+  Trip.find({departure, arrival}).then(dbData => {
+    const rep=[];
+    for (el of dbData) {
+      if (el.date > date1 && el.date < date2) {
+        rep.push(el);
+      }
     }
-  })
+    console.log(rep);
+    res.json({trips : rep})
+    })
+    
 })
 
 /* GET home page.
